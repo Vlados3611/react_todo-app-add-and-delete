8f8f476fffdcb566795f327416bfb24ddbcbce3f
Vladyslav Penkin <vy891078@gmail.com>
2023-06-12 00:35:45 +0300

    removed comment in App file

--- src/App.tsx
+++ src/App.tsx
@@ -361,12 +361,9 @@ export const App: React.FC = () => {
 
           <TodoList />
 
-          {/* Hide the footer if there are no todos */}
           {todos.length && <TodoFilter />}
         </div>
 
-        {/* Notification is shown in case of any error */}
-        {/* Add the 'hidden' class to hide the message smoothly */}
         {notificationList
           .slice(0, 3)
           .map((notification: Notification) => (
